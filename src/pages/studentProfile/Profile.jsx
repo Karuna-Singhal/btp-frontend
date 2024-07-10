ed98abe9b9fc883d82481740d013f37f06b2d55e
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2024-07-10 14:59:51 +0530

    logout

--- src/pages/studentProfile/Profile.jsx
+++ src/pages/studentProfile/Profile.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/24/solid";
 
 import LabTabs from "components/profileTab/Tab";
 import GeneralTab from "components/profileTab/GeneralTab";
@@ -8,7 +11,15 @@ import AssignmentTab from "components/profileTab/AssignmentTab";
 import SubjectTab from "components/profileTab/SubjectTab";
 
 function Profile() {
+  const navigate = useNavigate();
   const [value, setValue] = useState("general");
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/login");
+
+    toast.success("Logged out successfully");
+  };
 
   return (
     <div className="flex flex-col gap-[1rem] px-[2rem] py-[1rem] ">
@@ -18,9 +29,15 @@ function Profile() {
         </span>
         <LabTabs value={value} onChange={(e, newValue) => setValue(newValue)} />
 
-        <span className="text-primary-teal font-semibold text-[1.2rem]">
-          {localStorage.getItem("name")}
-        </span>
+        <div className="flex gap-4 items-center">
+          <span className="text-primary-teal font-semibold text-[1.2rem]">
+            {localStorage.getItem("name")}
+          </span>
+          <ArrowRightEndOnRectangleIcon
+            className="size-6 text-primary-red cursor-pointer"
+            onClick={logout}
+          />
+        </div>
       </div>
 
       <div className="mt-[1rem]">
